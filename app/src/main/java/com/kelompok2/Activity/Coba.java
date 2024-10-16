ce15fa07f334986c6654d3e91cc83f0ec40abcad
ferdio <ferdiodwi12@gmail.com>
2024-10-16 21:27:58 +0700

    ADD : -menambahkan import di class coba

--- app/src/main/java/com/kelompok2/Activity/Coba.java
+++ app/src/main/java/com/kelompok2/Activity/Coba.java
@@ -3,6 +3,10 @@ package com.kelompok2.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Coba {
 
